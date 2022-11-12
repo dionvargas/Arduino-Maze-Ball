fa33881d32e0b76e82e7f86408e6cd93b0970a92
dionvargas <dionathan_vargas@hotmail.com>
2022-11-12 10:18:33 -0300

    Update labirinto.ino
    Alterações no código para que seja fácil ajustar o ângulo inicial do labirinto.

--- labirinto/labirinto.ino
+++ labirinto/labirinto.ino
@@ -1,5 +1,11 @@
 #include <Servo.h>
 
+// Constantes de centro dos eixos e variação do angulo de rotação
+const int centroX = 90;
+const int centroY = 90;
+const int variacaoX = 90;
+const int variacaoY = 90;
+
 // Os pinos que estão ligados os potenciometros
 int pinoPotX = A4;
 int pinoPotY = A5;
@@ -7,6 +13,11 @@ int pinoPotY = A5;
 // Valor da leitura do potenciomentro
 int valorPotX;
 int valorPotY;
+
+// Angulo da posição do servo
+
+int anguloServoX = centroX;
+int anguloServoY = centroY;
 
 // Cria os objetos dos servos
 Servo servoX;
@@ -18,8 +29,8 @@ void setup() {
   servoY.attach(11);
 
   // Inicia os motores na posição zero
-  servoX.write(90);
-  servoY.write(90);
+  servoX.write(anguloServoX);
+  servoY.write(anguloServoY);
 }
 
 void loop() {
@@ -27,8 +38,8 @@ void loop() {
   valorPotY = analogRead(pinoPotY);
 
   // mapeia os valores do potenciometro (entre 0 e 1023) para os valores do servo (entre 0 e 180)
-  valorPotX = map(valorPotX, 0, 1023, 0, 180);
-  valorPotY = map(valorPotY, 0, 1023, 0, 180);
+  anguloServoX = map(valorPotX, 0, 1023, centroX - variacaoX, centroX + variacaoX);
+  anguloServoY = map(valorPotY, 0, 1023, centroY - variacaoY, centroY + variacaoY);
 
   // manda o valor para o servo
   servoX.write(valorPotX);
