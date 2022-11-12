065a7e20f138b86b721ce3ba4548bec661e91ba4
dionvargas <dionathan_vargas@hotmail.com>
2022-11-12 10:23:58 -0300

    Update labirinto.ino
    Correções de nomeclatura

--- labirinto/labirinto.ino
+++ labirinto/labirinto.ino
@@ -1,21 +1,20 @@
 #include <Servo.h>
 
-// Constantes de centro dos eixos e variação do angulo de rotação
+// Constantes de centro dos eixos e variação do ângulo de rotação
 const int centroX = 90;
 const int centroY = 90;
-const int variacaoX = 90;
-const int variacaoY = 90;
+const int variacaoX = 10;
+const int variacaoY = 10;
 
-// Os pinos que estão ligados os potenciometros
+// Os pinos que estão ligados os potenciômetros
 int pinoPotX = A4;
 int pinoPotY = A5;
 
-// Valor da leitura do potenciomentro
+// Valor da leitura do potenciômentro
 int valorPotX;
 int valorPotY;
 
-// Angulo da posição do servo
-
+// Ângulo da posição do servo
 int anguloServoX = centroX;
 int anguloServoY = centroY;
 
@@ -24,11 +23,11 @@ Servo servoX;
 Servo servoY;
 
 void setup() {
-  // Informa que pinos estão ligados a servos
+  // Informa em quais pinos estão ligados a servos
   servoX.attach(10);
   servoY.attach(11);
 
-  // Inicia os motores na posição zero
+  // Inicia os motores na posição central
   servoX.write(anguloServoX);
   servoY.write(anguloServoY);
 }
@@ -37,11 +36,11 @@ void loop() {
   valorPotX = analogRead(pinoPotX);
   valorPotY = analogRead(pinoPotY);
 
-  // mapeia os valores do potenciometro (entre 0 e 1023) para os valores do servo (entre 0 e 180)
+  // mapeia os valores do potenciômetro (entre 0 e 1023) para os valores do servo
   anguloServoX = map(valorPotX, 0, 1023, centroX - variacaoX, centroX + variacaoX);
   anguloServoY = map(valorPotY, 0, 1023, centroY - variacaoY, centroY + variacaoY);
 
-  // manda o valor para o servo
+  // grava o valor no servo
   servoX.write(valorPotX);
   servoY.write(valorPotY);
 }
